d46de710f0a097941f95eab5ed29897b923ac1da
Иван Чухманов <oollvanolloo@yandex.ru>
2026-06-18 17:39:04 +0300

    feat#7: Реализовал основную логику взаимодействия с Redis 1) Реализовал удаления долгоживущего токена 2) Реализовал проверку принадлежности токена пользователю 3) Реализовал логику добавления access token в черный список 4) Реализовал проверку наличия access token в черном списке

--- src/main/java/ru/bank/auth_service/infrastructure/RedisTokenStore.java
+++ src/main/java/ru/bank/auth_service/infrastructure/RedisTokenStore.java
@@ -14,11 +14,31 @@ public class RedisTokenStore {
     private static final String REFRESH_PREFIX = "refresh:";
     private static final String ACCESS_BLACK_LIST_PREFIX = "accessBlackList:";
 
-    // Сохранение в Redis Refresh токена
-    public void saveRefreshToken(UUID userId, String refreshToken, Long ttl){
+    // todo: Сохранение в Redis Refresh токена
+    public void saveRefreshToken(UUID userId, String refreshToken, long ttl){
         String key = REFRESH_PREFIX + userId;
         redisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(ttl));
     }
-
+    // todo: Удаление долгоживущего токена
+    public void deleteRefreshToken(UUID userId){
+        String key = REFRESH_PREFIX + userId;
+        redisTemplate.delete(key);
+    }
+    // todo: Проверка принадлежности токена пользователю
+    public boolean validateRefreshToken(UUID userId, String refreshToken){
+        String key = REFRESH_PREFIX + userId;
+        String stored = redisTemplate.opsForValue().get(key);
+        return refreshToken.equals(stored);
+    }
+    // todo: Добавления accessToken в черный список
+    public void addAccessTokenInBlackList(String accessToken, long ttl){
+        if(ttl < 0) return;
+        String key = ACCESS_BLACK_LIST_PREFIX + accessToken;
+        redisTemplate.opsForValue().set(key, "blacklisted", Duration.ofMillis(ttl));
+    }
+    // todo: Проверка наличия accessToken в черном списке
+    public boolean checkAccessTokenBlackList(String accessToken){
+        return Boolean.TRUE.equals(redisTemplate.hasKey(ACCESS_BLACK_LIST_PREFIX + accessToken));
+    }
 
 }
