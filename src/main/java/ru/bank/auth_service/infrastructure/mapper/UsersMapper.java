5cac707273488abc7ed18cd120ddc675c6470b5e
Иван Чухманов <oollvanolloo@yandex.ru>
2026-07-07 13:01:09 +0300

    feat: Добавил mapStruct для удобства взаимодействия с запросами и ответами

---
+++ src/main/java/ru/bank/auth_service/infrastructure/mapper/UsersMapper.java
@@ -0,0 +1,22 @@
+package ru.bank.auth_service.infrastructure.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.bank.auth_service.model.dto.request.RegistrationRequestDto;
+import ru.bank.auth_service.model.dto.response.RegistrationResponseDto;
+import ru.bank.auth_service.model.entity.Users;
+
+@Mapper(componentModel = "spring")
+public interface UsersMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Users toUserEntity(RegistrationRequestDto request);
+
+    @Mapping(target = "message", constant = "Пользователь зарегистрирован в системе")
+    RegistrationResponseDto toRegistrationResponse(Users users);
+
+}
