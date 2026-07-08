41d7b264ec49ab3a4c05913f961b0ee1ee5cbfa7
Иван Чухманов <oollvanolloo@yandex.ru>
2026-07-08 13:42:06 +0300

    refactor: Изменил название пакетов для генерации паролей, вывел их в util, + отредактировал название пакета для enum, переименовал его в enums + добавил проверку в enum для будущей логики

--- src/main/java/ru/bank/auth_service/model/entity/Users.java
+++ src/main/java/ru/bank/auth_service/model/entity/Users.java
@@ -9,8 +9,8 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import ru.bank.auth_service.model.enumerate.Role;
-import ru.bank.auth_service.model.enumerate.UserStatus;
+import ru.bank.auth_service.model.enums.Role;
+import ru.bank.auth_service.model.enums.UserStatus;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
--- src/main/java/ru/bank/auth_service/model/enumerate/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package ru.bank.auth_service.model.enumerate;
-
-public enum UserStatus {
-    PENDING, BLOCKED, DELETE, ACTIVE
-}
