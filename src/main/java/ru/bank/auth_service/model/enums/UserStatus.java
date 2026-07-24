d9c2db65ded3ffc08c89376145e4bec7564fd095
Иван Чухманов <oollvanolloo@yandex.ru>
2026-07-24 17:27:26 +0300

    fix: Исправил баг UserStatus. - Убрал ненужный статус удаленного пользователя

--- src/main/java/ru/bank/auth_service/model/enums/UserStatus.java
+++ src/main/java/ru/bank/auth_service/model/enums/UserStatus.java
@@ -1,17 +1,11 @@
 package ru.bank.auth_service.model.enums;
 
 public enum UserStatus {
-    PENDING, BLOCKED, DELETE, ACTIVE;
+    PENDING, BLOCKED, ACTIVE;
     public boolean isFirstLogin(){
         return this.equals(PENDING);
     }
     public boolean isBlocked(){
         return this.equals(BLOCKED);
-    }
-    public boolean isDeleted(){
-        return this.equals(DELETE);
-    }
-    public boolean isActive(){
-        return this.equals(ACTIVE);
     }
 }
